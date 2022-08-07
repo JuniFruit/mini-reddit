80fdf8adbde9fbce5581d924284eb2afaea5df85
JuniFruit@github.com <idiot228322@mail.ru>
2022-08-08 01:20:53 +0300

    added redux slice for subreddit data

--- client/src/features/subredditSlice.js
+++ client/src/features/subredditSlice.js
@@ -3,7 +3,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const fetchSubredditData = createAsyncThunk('subreddit/fetchSubredditData', async (subreddit) => {
 
     try {
-
+        // fetches the subreddit data. Expects non prefixed subreddit name, example 'worldnews'
         const response = await fetch(`/subreddit_data?subreddit=${subreddit}`);
         const data = await response.json();
         
@@ -20,21 +20,31 @@ const subredditSlice = createSlice({
     name: 'subreddit',
     initialState: {
         data: {},
-        errMessage: ''
+        errMessage: '',
+        isSubredditDataLoading: false
+
     },
     extraReducers: (builder) => {
         builder
             .addCase(fetchSubredditData.rejected, (state, action) => {
-                state.errMessage = action.payload.message
+                state.errMessage = action.payload.message;
+                state.isSubredditDataLoading = false;
             })
             .addCase(fetchSubredditData.fulfilled, (state, action) => {
-               
-                state.data = action.payload.data
-                state.errMessage = ''
+             
+                state.data[action.payload.data.data.display_name] = action.payload.data
+                state.errMessage = '';
+                state.isSubredditDataLoading = false;
+            })
+            .addCase(fetchSubredditData.pending, (state, action) => {
+                state.errMessage = '';
+                state.isSubredditDataLoading = true;
             })
     }
 })
 
-export const selectSubredditData = (state) => state.subredditSlice.data;
+export const selectSubredditData = (state) => state.subredditReducer.data;
+
+
 
 export default subredditSlice.reducer;
