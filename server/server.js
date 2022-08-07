80e2cad6bcd4db19d80e84f04fa16590ec945324
JuniFruit@github.com <idiot228322@mail.ru>
2022-08-08 01:11:42 +0300

    Added subreddit data and top subreddits api calls

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fetch = require('node-fetch')
 const cors = require('cors');
+const { response, application } = require("express");
 
 
 const redirect_uri = 'http://localhost:3000/reddit_login';
@@ -65,7 +66,7 @@ app.get("/reddit_login", async (req, res) => {
 app.get("/popular", async (req, res) => {
 
     try {
-
+        
         const response = await fetch('https://www.reddit.com/r/popular.json?geo_filter=TR');
         const data = await response.json();
         res.send({status: 200, data: data})
@@ -75,11 +76,24 @@ app.get("/popular", async (req, res) => {
     }
 })
 
+app.get('/check_user_geo', async (req, res) => {
+
+    try {
+
+        const response = await fetch('http://ip-api.com/json/');
+        const data = await response.json();
+        res.send({status: 200, data: data})
+
+    } catch (e) {
+        res.send({status: 'error', message: `Couldn't retrieve the data. Reason: ${e}`})
+    }
+})
+
 app.get('/news', async (req,res) => {
     
     try {
-
-        const response = await fetch('https://www.reddit.com/search.json?q=news');
+        const country = req.query.country;
+        const response = await fetch(`https://www.reddit.com/search.json?q=${country}%20news&source=trending`);
         const data = await response.json();
         res.send({status: 200, data: data});
 
@@ -91,7 +105,7 @@ app.get('/news', async (req,res) => {
 app.get('/subreddit_data', async (req, res) => {
     
     try {
-        console.log('Server reached')
+        
         const query = req.query.subreddit;
         const response = await fetch(`https://www.reddit.com/r/${query}/about.json`);
         const data = await response.json();
@@ -99,6 +113,23 @@ app.get('/subreddit_data', async (req, res) => {
 
     } catch(e) {
         res.send({status: 'error', message: `Couldn't retrieve the data. Reason: ${e}`});
+    }
+})
+
+app.get('/top_subreddits', async (req, res) => {
+    
+    try {
+
+        const response = await fetch('https://www.reddit.com/subreddits.json');
+        const data = await response.json();
+        const slicedChildren = data.data.children.slice(0, 7);
+
+        res.send({status: 200, data: slicedChildren})
+
+    } catch (e) {
+
+        res.send({status: 'error', message: `Couldn't retrieve data. Reason: ${e}`});s
+
     }
 })
 
