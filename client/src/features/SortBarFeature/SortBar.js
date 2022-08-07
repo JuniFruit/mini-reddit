bbc78d1f4b88e913f5a6acee7d6042bf6471e337
JuniFruit@github.com <idiot228322@mail.ru>
2022-08-08 01:18:11 +0300

    updated styles

--- client/src/features/SortBarFeature/SortBar.js
+++ client/src/features/SortBarFeature/SortBar.js
@@ -1,29 +1,29 @@
 import './SortBar.css'
-import { images } from '../../assets/images'
 import Icon from '../../assets/icons'
+import { Link } from 'react-router-dom'
 
 export const SortBar = () => {
 
     return (
         <div className='sortBar'>
-            <h4>Popular posts</h4>
+            
             <div className='sortBar-container'>
                 <div className='sortBar-buttons'>
 
-                    <a href='' className='sortBar-button' >
+                    <Link to={'/'} className='sortBar-button' >
                         <Icon icon="hot-svgrepo-com" className="ic_home icons" />
 
                         <span>Hot</span>
-                    </a>
-                    <a href='' className='sortBar-button' >
+                    </Link>
+                    <Link to={'/'} className='sortBar-button' >
                         <Icon icon="play" className="ic_play icons" />
                         <span>Test</span>
-                    </a>
-                    <a href='' className='sortBar-button' >
+                    </Link>
+                    <Link to={'/'} className='sortBar-button' >
                         <span>Everywhere</span>
                         <Icon icon="compass" className="ic_location icons" />
                         
-                    </a>
+                    </Link>
 
                 </div>
                 <div className='sortBar-empty'></div>
