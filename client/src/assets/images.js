d09a5f6b9ab8a3c466e59409d4e5049637111811
JuniFruit@github.com <idiot228322@mail.ru>
2022-09-13 23:23:08 +0300

    prebuild commit

--- client/src/assets/images.js
+++ client/src/assets/images.js
@@ -1,14 +1,10 @@
 import logoChar from './logo_char.png';
-import logoReddit from './logo_reddit.png';
-import testImg from './logo512.png'
 import bannerImg from './banner-background.png'
 
 import defaultCommunityImg from './defaultCommunityImg.png'
 
 export const images = {
-    logoReddit,
     logoChar,
-    testImg,
     defaultCommunityImg,
     bannerImg
 }
